5c64921ca2c53918a8a5143ca8cc5d730ddd75d3
FoxTheUnicorn <weedy193@gmail.com>
2024-01-12 14:14:57 +0100

    Added a Poof when enemies Despawn
    Added a Poof when enemies Despawn

--- Assets/Scripts/Enemies/RegularEnemy.cs
+++ Assets/Scripts/Enemies/RegularEnemy.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class RegularEnemy : MonoBehaviour {
+    [SerializeField] float DespawnTime = 5.0f;
     private ParticleSystem DeathParticles;
     private Rigidbody EnemyRigidbody;
+    private Vector3 torque;
 
      void Start()
     {
@@ -16,10 +18,21 @@ public class RegularEnemy : MonoBehaviour {
     {
         DeathParticles.Play();
         EnemyRigidbody.freezeRotation = false;
+        EnemyRigidbody.AddTorque(RandomTorque());
+        Invoke("CommenceDeath", DespawnTime);
     }
 
-    public void KillObject()
+    private Vector3 RandomTorque()
     {
-       this.gameObject.
+        torque.x = Random.Range(-200, 200);
+        torque.y = Random.Range(-200, 200);
+        torque.z = Random.Range(-200, 200);
+        return torque;
+    }
+    public void CommenceDeath()
+    {
+        DeathParticles.Play();
+        Destroy(gameObject, DeathParticles.main.duration);
+        transform.GetChild(0).localScale = new Vector3(0, 0, 0);
     }
 }
