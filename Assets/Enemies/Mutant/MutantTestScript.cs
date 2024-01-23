d2a03bab05537d674bf29e8f89e21dbb409e5cd0
FoxTheUnicorn <weedy193@gmail.com>
2024-01-23 15:50:53 +0100

    Added Mutant+ Animations

---
+++ Assets/Enemies/Mutant/MutantTestScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MutantTestScript : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void Update()
+    {
+        TogglePresses("z", "RandomWalk");
+        TogglePresses("x", "FoundEnemy");
+        TogglePresses("c", "RandomRoar");
+        TogglePresses("v", "ReachedEnemy");
+    }
+
+    void TogglePresses(string key, string boolarg)
+    {
+        if (Input.GetKey(key) && !animator.GetBool(boolarg))
+        {
+            animator.SetBool(boolarg, true);
+        }
+        else if (!Input.GetKey(key) && animator.GetBool(boolarg))
+        {
+            animator.SetBool(boolarg, false);
+        }
+    }
+}
