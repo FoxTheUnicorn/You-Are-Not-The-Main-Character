22df43ec35e28956aebe14a51e94ebaaa3b66e28
FoxTheUnicorn <weedy193@gmail.com>
2024-01-25 19:19:18 +0100

    Modified Volume Script so you dont blast your eardrums out by accident

--- Assets/Scripts/UI/VolumeSlider.cs
+++ Assets/Scripts/UI/VolumeSlider.cs
@@ -5,16 +5,18 @@ using UnityEngine.Audio;
 
 public class VolumeSlider : SliderPercentage
 {
+    [SerializeField] private float MinVolume = -50.0f;
+    [SerializeField] private float MaxVolume = 5.0f;
     public AudioMixer mixer;
     new private void Start()
     {
         base.Start();
-        mixer.SetFloat("volume", Mathf.Lerp(-80.0f, 20.0f, value));
+        mixer.SetFloat("volume", Mathf.Lerp(MinVolume, MaxVolume, value));
     }
 
     new public void UpdatePercentageSavePref()
     {
         base.UpdatePercentageSavePref();
-        mixer.SetFloat("volume", Mathf.Lerp(-80.0f, 20.0f, value));
+        mixer.SetFloat("volume", Mathf.Lerp(MinVolume, MaxVolume, value));
     }
 }
