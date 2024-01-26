076ed29c7ca1e8963a4ff1a6bb67c5d060934c9c
joernschw <114585849+joernschw@users.noreply.github.com>
2024-01-26 01:13:35 +0100

    Fixed NavMeshAgent

--- Assets/Scripts/EnemyAttackController.cs
+++ Assets/Scripts/EnemyAttackController.cs
@@ -12,11 +12,9 @@ public class EnemyAttackController : MonoBehaviour
         HashSet<Collider> colliders = PunchHitbox.Hit();
         foreach (Collider collider in colliders)
         {
-            Debug.Log(collider.gameObject.name);
             Character otherCharacter;
             if ((((otherCharacter = (collider.GetComponent("HeroCharacter") as Character)) != null) || ((otherCharacter = (collider.GetComponent("PlayerCharacter") as Character)) != null)) && ownCharacter.getCanHit())
             {
-                Debug.Log("Hit " + collider.gameObject.name);
                 ownCharacter.hitEnemy(otherCharacter);
             }
         }
